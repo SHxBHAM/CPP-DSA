35594ea04a3c6be9ddf63b1b93d56c0d8c9c4e1a
SHxBHAM <shubhammishra41880@gmail.com>
2023-10-24 19:12:05 +0530

    if else problem for positive or negative

---
+++ 1_if_else.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"please enter your value:";
+    cin>>n;
+    if(n>0){
+        cout<<"the provided value is positive.";
+    }
+    else{
+        cout<<"provided value is negative.";
+    }
+    return 0;
+}
